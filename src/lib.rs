ac579e504aed67a007ad2c7530594bde31523566
Dave Herman <dherman@mozilla.com>
2015-09-20 22:19:26 -0700

    s/FunctionCallbackInfo/Call/g

--- src/lib.rs
+++ src/lib.rs
@@ -6,22 +6,22 @@ use std::ffi::CString;
 
 // Rust implementations of Node functions must be linked without mangled names.
 #[no_mangle]
-pub extern fn make_a_pi(info: &mut FunctionCallbackInfo) {
+pub extern fn make_a_pi(call: &mut Call) {
     // Allocating JS values requires executing in a Scope.
     Scope::run(|scope| {
         // Set the current JS function activation's return value to an allocated number.
-        info.set_return(scope.number(3.14));
+        call.set_return(scope.number(3.14));
     });
 }
 
 #[no_mangle]
-pub extern fn make_an_array(info: &mut FunctionCallbackInfo) {
+pub extern fn make_an_array(call: &mut Call) {
     let result = Scope::run(|scope| {
         let mut array = scope.array(3);
         array.set(0, scope.integer(17));
         array.set(1, scope.integer(42));
         array.set(2, scope.integer(1999));
-        info.set_return(array);
+        call.set_return(array);
     });
     //println!(">>> this is Rust speaking: I have a Rust value: {:?}", result);
 }
