69d0e39f3964e7ac821b1d884a43b0d8b308b9a5
Dave Herman <dherman@mozilla.com>
2015-10-07 21:33:03 -0700

    update for newer, safer nanny v0.0.3

--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,78 @@
 extern crate nanny;
 
-use nanny::*;
+use nanny::vm::Call;
+use nanny::value::Object;
+use nanny::local::Local;
+use nanny::scope::Scope;
 
 use std::ffi::CString;
 
 // Rust implementations of Node functions must be linked without mangled names.
 #[no_mangle]
-pub extern fn make_a_pi(call: &mut Call) {
+pub extern fn make_a_pi(call: &Call) {
     // Allocating JS values requires executing in a Scope.
-    Scope::run(|scope| {
+    call.realm().scoped(|scope| {
         // Set the current JS function activation's return value to an allocated number.
-        call.set_return(scope.number(3.14));
+        call.activation().set_return(scope.number(3.14));
     });
 }
 
 #[no_mangle]
-pub extern fn make_an_array(call: &mut Call) {
-    Scope::run(|scope| {
+pub extern fn make_an_array(call: &Call) {
+    call.realm().scoped(|scope| {
         let mut array = scope.array(3);
         array.set(0, scope.integer(17));
         array.set(1, scope.integer(42));
         array.set(2, scope.integer(1999));
-        call.set_return(array);
+        call.activation().set_return(array);
     });
     //println!(">>> this is Rust speaking: I have a Rust value: {:?}", result);
+}
+
+#[no_mangle]
+pub extern fn make_a_number(call: &Call) {
+    call.realm().scoped(|scope| {
+        let x = scope.integer(1999);
+        let y = scope.nested(|_| {
+            17
+        });
+        println!("y = {}", y);
+        call.activation().set_return(x);
+    });
+}
+
+#[no_mangle]
+pub extern fn escape_example(call: &Call) {
+    call.realm().scoped(|scope| {
+        let mut x = None;
+        scope.chained(|scope| {
+            let mut array = scope.array(2);
+            array.set(0, scope.integer(42));
+            array.set(1, scope.number(6.28));
+            x = Some(scope.escape(array));
+        });
+        call.activation().set_return(x.unwrap());
+    });
+}
+
+#[no_mangle]
+pub extern fn should_panic(call: &Call) {
+    call.realm().scoped(|outer| {
+        let mut x = outer.integer(0);
+        let y = outer.nested(|_| {
+            // panic: outer scope is inactive!
+            x = outer.integer(1999);
+            17
+        });
+        println!("y = {}", y);
+        call.activation().set_return(x);
+    });
 }
 
 /*
 // This produces a lifetime error as expected:
 fn naughty<'a>() -> Local<'a, Integer> {
-    Scope::run(|scope| {
+    call.realm().scoped(|scope| {
         scope.integer(17)
     })
 }
@@ -41,4 +84,7 @@ fn naughty<'a>() -> Local<'a, Integer> {
 pub extern fn node_main(mut module: Local<Object>) {
     module.export(&CString::new("make_a_pi").unwrap(), make_a_pi);
     module.export(&CString::new("make_an_array").unwrap(), make_an_array);
+    module.export(&CString::new("make_a_number").unwrap(), make_a_number);
+    module.export(&CString::new("escape_example").unwrap(), escape_example);
+    module.export(&CString::new("should_panic").unwrap(), should_panic);
 }
